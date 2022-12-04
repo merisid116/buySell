9815b41bb7e5da03a6f5503c91a7c821e34db1da
Mari <merisid2399@mail.ru>
2022-12-04 20:31:06 +0300

    Initial commit

---
+++ src/main/java/com/example/buySell/BuySellApplication.java
@@ -0,0 +1,10 @@
+package com.example.buySell;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BuySellApplication {
+	public static void main(String[] args) {SpringApplication.run(BuySellApplication.class, args);}
+
+}
---
+++ src/main/java/com/example/buySell/models/enums/Role.java
@@ -0,0 +1,5 @@
+package com.example.buySell.models.enums;
+
+public enum Role {
+    ROLE_USER, ROLE_ADMIN
+}
